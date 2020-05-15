5a2fb12d0e9358a1393bac8a8e48c9c1068cfab8
Mozl <louis.moselhi@bbc.co.uk>
2020-05-15 11:08:31 +0100

    looking at next js features

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const About = () => {
+  return (
+    <>
+      <div>About</div>
+      <Link><a href='/'>Home</a></Link>
+    </>
+  )
+}
+
+export default About;
