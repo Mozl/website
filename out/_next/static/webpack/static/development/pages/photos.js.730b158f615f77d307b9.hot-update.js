8e3ab1113a3fa542e156938027ca9bac8e94adca
Mozl <louis.moselhi@bbc.co.uk>
2020-05-15 13:36:45 +0100

    Deployment to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/photos.js.730b158f615f77d307b9.hot-update.js
@@ -0,0 +1,55 @@
+webpackHotUpdate("static/development/pages/photos.js",{
+
+/***/ "./pages/photos.js":
+/*!*************************!*\
+  !*** ./pages/photos.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _this = undefined,
+    _jsxFileName = "/Users/mosell01/projects/website/pages/photos.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+// import Link from '../components/Link'
+
+
+var Photos = function Photos() {
+  return __jsx(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 7
+    }
+  }, "Photos"), __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8,
+      columnNumber: 7
+    }
+  }, __jsx("a", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8,
+      columnNumber: 22
+    }
+  }, "Home")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Photos);
+
+/***/ })
+
+})
+//# sourceMappingURL=photos.js.730b158f615f77d307b9.hot-update.js.map
