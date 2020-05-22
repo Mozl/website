82a32a3741d57df19ebc9ed83bf19144c226efe7
Mozl <louis.moselhi@bbc.co.uk>
2020-05-22 14:53:53 +0100

    Deployment to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "856e6f0db0b6954265b8";
+/******/ 	var hotCurrentHash = "6dc96ff15153ac876b4c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
