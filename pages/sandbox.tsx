4a8629f26231b85681e0a96177d0f66b718c59f2
Mozl <louis.moselhi@bbc.co.uk>
2021-02-15 09:22:40 +0000

    spinning 3d logo from blender

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useRef, Suspense } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Nav from '../components/Nav';
 import Menu from '../components/Menu';
 import SpinningMesh from '../components/SpinningMesh';
 import { toggleMenu } from '../redux/actions/menuActions';
-import { Canvas } from 'react-three-fiber';
-// import { softShadows } from 'drei';
+import { Canvas, useLoader } from 'react-three-fiber';
+import { useFrame } from 'react-three-fiber';
 
 interface RootState {
   isMenuOpen: {
@@ -13,7 +13,28 @@ interface RootState {
   };
 }
 
-// softShadows();
+let GLTFLoader;
+function Model(props) {
+  GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader').GLTFLoader;
+  const group = useRef();
+  const gltf = useLoader(GLTFLoader, '/pink-logo-3d-2.glb');
+  const mesh = useRef<THREE.Mesh>();
+  useFrame(() => (mesh.current.rotation.y += 0.01));
+  return (
+    // <group ref={group} {...props} dispose={null}>
+    //   <mesh
+    //     material={gltf.materials.lambert2SG}
+    //     geometry={gltf.nodes.Object_3.geometry}
+    //     scale={[0.03, 0.03, 0.03]}
+    //     material-color=""
+    //   />
+    //   <mesh material={gltf.materials.lambert2SG} geometry={gltf.nodes.Object_4.geometry} material-color="red" />
+    // </group>
+    <mesh receiveShadow ref={mesh}>
+      <primitive object={gltf.scene} dispose={null} />
+    </mesh>
+  );
+}
 
 const Sandbox: React.FC = () => {
   const isMenuOpen = useSelector((state: RootState) => state.isMenuOpen);
@@ -21,6 +42,7 @@ const Sandbox: React.FC = () => {
   const onToggleMenu = () => {
     dispatch(toggleMenu());
   };
+
   return (
     <>
       {/* <Nav isMenuOpen={isMenuOpen} onToggleMenu={onToggleMenu} />
@@ -47,11 +69,14 @@ const Sandbox: React.FC = () => {
               <planeBufferGeometry attach="geometry" args={[100, 100]} />
               <shadowMaterial attach="material" opacity={0.3} />
             </mesh>
+            <Suspense fallback={null}>
+              <Model />
+            </Suspense>
           </group>
 
-          <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} />
-          <SpinningMesh position={[-2, 1, -5]} />
-          <SpinningMesh position={[5, 1, -2]} />
+          {/* <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} /> */}
+          {/* <SpinningMesh position={[-2, 1, -5]} /> */}
+          {/* <SpinningMesh position={[5, 1, -2]} /> */}
         </Canvas>
       </div>
       <style jsx>{`
