89003527f77f8fa211452943944a7e0e96b36e72
Mozl <louis.moselhi@bbc.co.uk>
2021-02-19 22:19:26 +0000

    skybox activated 💥

---
+++ components/Skybox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useThree } from 'react-three-fiber';
+
+let CubeTextureLoader;
+const Skybox = () => {
+  CubeTextureLoader = require('three').CubeTextureLoader;
+
+  const urls = [
+    'cubemap/posx.jpg',
+    'cubemap/negx.jpg',
+    'cubemap/posy.jpg',
+    'cubemap/negy.jpg',
+    'cubemap/posz.jpg',
+    'cubemap/negz.jpg'
+  ];
+
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+  const texture = loader.load(urls);
+  scene.background = texture;
+  return null;
+};
+
+export default Skybox;
