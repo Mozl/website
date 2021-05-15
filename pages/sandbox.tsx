4aeb1d84451615cfeebc65c12bc90aa9e0fc2766
Mozl <moselhil@gmail.com>
2021-05-15 17:32:53 +0100

    rm gui

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Model from '../components/Model';
 import ModelViewer from '../components/ModelViewer';
-import { Controls } from 'react-three-gui';
 import Sphere from '../components/Sphere';
 import Skybox from '../components/Skybox';
 import EnvMapSphere from '../components/EnvMapSphere';
@@ -19,15 +18,14 @@ const Sandbox: React.FC = () => {
     <>
       <div className="three">
         {typeof window !== 'undefined' && (
-          <Controls.Provider>
-            <ModelViewer includeGui={true}>
+          <>
+            <ModelViewer>
               {/* <Model modelPath="/react-logo-3d-3.glb" /> */}
               {/* <Sphere /> */}
               <EnvMapSphere />
               <Skybox urls={urls} />
             </ModelViewer>
-            <Controls />
-          </Controls.Provider>
+          </>
         )}
       </div>
       <style jsx>{`
